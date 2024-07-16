b945d5b63a93f1435c7466d9ab69eaea409469fd
KSOL <ksol95@ya.ru>
2024-07-16 18:43:38 +0400

    Ревю 2: исправил хуки

--- src/components/article-params-form/hooks/useClickOutside.tsx
+++ src/components/article-params-form/hooks/useClickOutside.tsx
@@ -6,17 +6,6 @@ type UseClickOutside = {
 	onClick: () => void;
 };
 
-const itOutside = (element: Node, root: HTMLDivElement) => {
-	if (!root.contains(element)) {
-		let parent = element.parentElement;
-		while (parent !== null) {
-			parent = parent.parentElement;
-			if (parent == document.body) return true;
-		}
-	}
-	return parent === null ? true : false;
-};
-
 export const useClickOutsideForm = ({
 	isOpen,
 	rootRef,
@@ -25,16 +14,16 @@ export const useClickOutsideForm = ({
 	useEffect(() => {
 		const handleClick = (event: MouseEvent) => {
 			const { target } = event;
-			const root = rootRef.current;
-			const outside = itOutside(target as Node, root as HTMLDivElement);
-			if (target instanceof Node && outside) onClick?.();
+			if (target instanceof Node && !rootRef.current?.contains(target)) {
+				isOpen && onClick?.();
+			}
 		};
 
 		//Слушаем событие только при открытой форме
-		isOpen && window.addEventListener('click', handleClick);
+		isOpen && window.addEventListener('mousedown', handleClick);
 
 		return () => {
-			window.removeEventListener('click', handleClick);
+			window.removeEventListener('mousedown', handleClick);
 		};
 	}, [isOpen]);
 };
